5d0434c2c49a8bd0eee280b30c30daf44ac2698e
이상재 <tkdwo7699@gmail.com>
2024-02-25 19:15:10 +0900

    🎉Init: 프로젝트 생성

---
+++ band43/src/test/java/com/example/band43/Band43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.band43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Band43ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
