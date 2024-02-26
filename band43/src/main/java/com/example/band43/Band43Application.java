af953140b13b20e6f58a9235be74e2454b2d8e45
Sangjae Lee <tkdwo7699@gmail.com>
2024-02-26 13:14:03 +0900

    🎉Init: 프로젝트 생성 (#1)
    * 🎉Init: 프로젝트 생성
    
    * ✨Feat: Base 엔티티 생성 ( 생성시간, 수정시간 )
    
    * 🗒️Docs: PR 템플릿 추가

---
+++ band43/src/main/java/com/example/band43/Band43Application.java
@@ -0,0 +1,13 @@
+package com.example.band43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Band43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Band43Application.class, args);
+	}
+
+}
